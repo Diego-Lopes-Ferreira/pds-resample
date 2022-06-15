0c9ba42cd3e8cf3eadcf9bb891c52bb6bbc68990
Diego <dglpferreira@gmail.com>
2022-06-15 11:49:44 -0300

    refactor: rename x1 and x2
      This variables are about the left and right side of the signal
      so it makes more sense to call them xEsquerdo and xDireito
    
      Esquerdo = Left
      Direito  = Right

--- dnsample.m
+++ dnsample.m
@@ -8,29 +8,29 @@ posZero = find (n==0);
 
 % LADO NEGATIVO
 % Inverte o eixo X negativo: do inicio ate o 0
-x1 = fliplr(x(1:posZero));
+xEsquerdo = fliplr( x( 1:posZero ) );
 % Faz o downsample da parte da direita utilizando a funcao do matlab
-yEsquerdo = downsample(x1,M);
+yEsquerdo = downsample(xEsquerdo, M);
 % Inverte novamente para voltar essas amostras para o lado negativo
-yEsquerdo = fliplr(yEsquerdo);
+yEsquerdo = fliplr( yEsquerdo );
 % Monta a funcao m de saida (que e a funcao n de entrada, so que do inicio
 % ate o 0) utilizando um T ("tamanho(1,2)") diferente
-tamanho = size(yEsquerdo);
+tamanho = size( yEsquerdo );
 mEsquerdo = -fliplr( 1:tamanho(1,2)-1 );
 
 % LADO POSITIVO
 % Pega o lado positivo: do 0 ate o fim
-x2 = x(posZero:end);
-yDireito = downsample(x2,M);
+xDireito = x(posZero:end);
+yDireito = downsample(xDireito, M);
 
 % Monta a funcao m de saida (que e a funcao n de entrada, so que do 0
 % ate o fim) utilizando um T ("tamanho(1,2)") diferente
-tamanho = size(yDireito);
+tamanho = size( yDireito );
 mDireito = 0:tamanho(1,2)-1;
 
 % Por fim, retorna os valores de y e m:
 % - y = yEsquerdo + yDireito
 % ** remove o ultimo elemento do yEsquerdo que e o zero
-y = [yEsquerdo(1:end-1), yDireito];
+y = [ yEsquerdo(1:end-1), yDireito ];
 % - m = mEsquerdo + mDireito
-m = [mEsquerdo, mDireito];
+m = [ mEsquerdo, mDireito ];
